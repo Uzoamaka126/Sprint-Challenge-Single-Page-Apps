77214c533d1838ddc6edb82f5e08f9eb5281dbb8
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2019-09-20 14:49:56 +0100

    Second

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import React, { useState } from "react";
-import { Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from "./components/Header.js";
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
+import axios from 'axios';
 
+const api = 'https://rickandmortyapi.com/api/character/';
 
 export default function App() {
+  const [ charactersData, setCharactersData ] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 
-  const [allCharactersList, setAllCharacters] = useState(null);
-  const addToCharacterList = character => {
-    setAllCharacters( [...allCharactersList, character] );
+  // const addToCharacterList = character => {
+  //   setAllCharacters( [...allCharactersList, character] );
+  // };
+  const onSearch = evt => {
+    setSearchInput(evt.target.value)
   };
 
-  return (
-    <main>
-      <Header />
-      <Route exact path='/' component={WelcomePage} />
-      <Route  exact path='/characters_page' component={CharacterList} />
-    </main>
-  );
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+      axios
+        .get(api)
+        .then(res => {
+          // debugger
+          // console.log(res);
+          // console.log(res.data);
+          // console.log(res.data.results);
+          // debugger
+          setCharactersData(res.data.results);
+          console.log(charactersData)
+        })
+        .catch(error => {
+          console.log('Server Error', error);
+        });
+  }, []);
+
+  if (charactersData) {
+    return (
+     <Router>
+        <main>
+          <Header />
+          {/* <SearchForm onSearch={onSearch} searchInput={searchInput} /> */}
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/welcome">Welcome</Link>
+            <Link to="/characters">List of Characters</Link>
+          </nav>
+        <Route exact path='/welcome' component={WelcomePage} />
+        <Route  exact path='/characters' 
+          render={() => <CharacterList characterList={charactersData}/>}
+        />
+        {/* <Route  exact path='/charact' component={CharacterList} /> */}
+
+      </main>
+     </Router>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Character Data Loading...</h1>
+      </div>
+    )
+  }
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,44 +5,29 @@ import CharacterCard from './CharacterCard';
 
 export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-  const api = 'https://rickandmortyapi.com/api/character/';
-
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    const getCharacters = () => {
-      axios
-        .get(api)
-        .then(res => {
-          // debugger
-          setCharacters(res.data.results);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    console.log(characters)
-
-    getCharacters();
-  }, []);
+  // const { name, species, status, gender, image } = props.characters;
 
   return (
     <section className="character-list">
-      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
-      {characters.map(character => (
-        // debugger
-        <Link to={`/character/${character.id}`}>
-          <CharacterDetails key={character.id} character={character} />
-        </Link>
-      ))}
-    </section>
+    {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+      <h2>
+      {/* {props.characters.map(character => (
+          <CharacterCard 
+            key={character.id} 
+            name={character} 
+            species={character.species}
+            status={character.status}
+            gender={character.gender}
+            imgUrl={character.image}
+            />
+      ))} */}
+      </h2>
+  </section>
   );
 }
 
-function CharacterDetails({ character }) {
-  return (
-    <CharacterCard character={character} />
-  )
-}
+// function CharacterDetails({ character }) {
+//   return (
+//     <CharacterCard character={character} />
+//   )
+// }
